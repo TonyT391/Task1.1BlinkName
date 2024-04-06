a66a7fc80847f0f2080992096ef8d6fe1e2c9e20
TonyT391 <113053003+TonyT391@users.noreply.github.com>
2024-04-07 01:54:58 +1000

    Add files via upload

---
+++ terrific_borwo1.ino
@@ -0,0 +1,55 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+  for(int i = 0; i<3; i++)
+  {
+  	digitalWrite(LED_BUILTIN, HIGH);
+  	delay(1000);
+  	digitalWrite(LED_BUILTIN, LOW);
+  	delay(1000); 
+  }
+ 	
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200); 
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200); 
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+  
+  
+}
+
+
+
+    
